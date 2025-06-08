0282f1f7f88823c7eec9b96e45e51f188ce8d601
vteeev <piotr.nie00@gmail.com>
2025-06-08 21:44:21 +0200

    Add Home View Controller

--- worbench/Controllers/HomeController.cs
+++ worbench/Controllers/HomeController.cs
@@ -1,20 +1,37 @@
+namespace worbench.Controllers;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using worbench.Models;
+using worbench.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
 
-namespace worbench.Controllers;
-
+[Authorize]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager)
     {
         _logger = logger;
+        _userManager = userManager;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+        var user = await _userManager.GetUserAsync(User);
+        if (user != null)
+        {
+            var roles = await _userManager.GetRolesAsync(user);
+            if (roles.Contains("Admin"))
+                return RedirectToAction("Index", "Admin");
+            if (roles.Contains("Mechanik"))
+                return RedirectToAction("Dashboard", "Mechanic");
+            // Domyślnie każdy inny użytkownik jest klientem
+            return RedirectToAction("Dashboard", "Client");
+        }
         return View();
     }
 
