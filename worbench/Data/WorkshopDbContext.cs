5983fd9e7e55075506d5375881a8349396683b9d
vteeev <piotr.nie00@gmail.com>
2025-06-08 21:42:50 +0200

    Data

---
+++ worbench/Data/WorkshopDbContext.cs
@@ -0,0 +1,43 @@
+﻿namespace worbench.Data;
+using worbench.Models;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+public class WorkshopDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, int>
+{
+    public WorkshopDbContext(DbContextOptions<WorkshopDbContext> options) : base(options) { }
+
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Vehicle> Vehicles { get; set; }
+    public DbSet<Part> Parts { get; set; }
+    public DbSet<ServiceOrder> ServiceOrders { get; set; }
+    public DbSet<ServiceTask> ServiceTasks { get; set; }
+    public DbSet<UsedPart> UsedParts { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Part>()
+            .Property(p => p.UnitPrice)
+            .HasPrecision(10, 2); // np. 10 cyfr, z czego 2 po przecinku
+
+        modelBuilder.Entity<ServiceTask>()
+            .Property(t => t.LaborCost)
+            .HasPrecision(10, 2);
+
+        // Naprawa: relacja Comment -> ServiceOrder bez kaskadowego usuwania
+        modelBuilder.Entity<Comment>()
+            .HasOne(c => c.ServiceOrder)
+            .WithMany(so => so.Comments)
+            .HasForeignKey(c => c.ServiceOrderId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        // Unikalny indeks na UserId w Customer
+        modelBuilder.Entity<Customer>()
+            .HasIndex(c => c.UserId)
+            .IsUnique();
+    }
+
+}
