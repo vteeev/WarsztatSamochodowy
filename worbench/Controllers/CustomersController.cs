aac2f15fd66936fb6fa3531404c83edcbd00623a
vteeev <piotr.nie00@gmail.com>
2025-06-08 21:41:50 +0200

    Add Controller Customers and View

---
+++ worbench/Controllers/CustomersController.cs
@@ -0,0 +1,164 @@
+﻿namespace worbench.Controllers;
+using Microsoft.EntityFrameworkCore;
+using worbench.Models;
+using worbench.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Linq;
+using System;
+
+public class CustomersController : Controller
+{
+    private readonly WorkshopDbContext _context;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public CustomersController(WorkshopDbContext context, UserManager<ApplicationUser> userManager)
+    {
+        _context = context;
+        _userManager = userManager;
+    }
+    
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.Customers.ToListAsync());
+    }
+    
+
+    public async Task<IActionResult> MyProfile()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        var customer = await _context.Customers.FirstOrDefaultAsync(c => c.UserId == user.Id);
+        if (customer == null)
+        {
+            return RedirectToAction("Create");
+        }
+        var vehicles = await _context.Vehicles.Where(v => v.CustomerId == customer.Id).ToListAsync();
+        ViewBag.Vehicles = vehicles;
+        return View(customer);
+    }
+    
+    // GET: Customers/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null) return NotFound();
+
+        var customer = await _context.Customers
+            .FirstOrDefaultAsync(m => m.Id == id);
+
+        if (customer == null) return NotFound();
+
+        return View(customer);
+    }
+
+    // GET: Customers/Create
+    public async Task<IActionResult> Create()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        var existingCustomer = await _context.Customers.FirstOrDefaultAsync(c => c.UserId == user.Id);
+        if (existingCustomer != null)
+        {
+            return RedirectToAction("MyProfile");
+        }
+        return View();
+    }
+
+    // POST: Customers/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("FirstName,LastName,Email,Phone,Address")] Customer customer)
+    {
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
+        {
+            return RedirectToAction("Login", "Account");
+        }
+
+        // Sprawdź, czy użytkownik już ma klienta
+        var existingCustomer = await _context.Customers.FirstOrDefaultAsync(c => c.UserId == user.Id);
+        if (existingCustomer != null)
+        {
+            // Możesz przekierować do profilu lub edycji
+            return RedirectToAction("MyProfile");
+        }
+
+        customer.UserId = user.Id;
+        if (ModelState.IsValid)
+        {
+            _context.Add(customer);
+            await _context.SaveChangesAsync();
+            // Przekierowanie na Client/Dashboard po utworzeniu klienta
+            return RedirectToAction("Dashboard", "Client");
+        }
+        return View(customer);
+    }
+
+    // GET: Customers/Edit/5
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null) return NotFound();
+
+        var customer = await _context.Customers.FindAsync(id);
+        if (customer == null) return NotFound();
+
+        return View(customer);
+    }
+
+    // POST: Customers/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Email,Phone,Address")] Customer customer)
+    {
+        if (id != customer.Id) return NotFound();
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                _context.Update(customer);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CustomerExists(customer.Id)) return NotFound();
+                else throw;
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        return View(customer);
+    }
+
+    // GET: Customers/Delete/5
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null) return NotFound();
+
+        var customer = await _context.Customers
+            .FirstOrDefaultAsync(m => m.Id == id);
+
+        if (customer == null) return NotFound();
+
+        return View(customer);
+    }
+
+    // POST: Customers/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var customer = await _context.Customers.FindAsync(id);
+        if (customer != null)
+        {
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+        }
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool CustomerExists(int id)
+    {
+        return _context.Customers.Any(e => e.Id == id);
+    }
+}
