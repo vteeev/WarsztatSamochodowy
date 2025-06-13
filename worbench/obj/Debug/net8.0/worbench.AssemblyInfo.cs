fd168af67ad6b9c2f38a004d5fbc801273871e33
Damian9413 <damian.skiba.9413.2@gmail.com>
2025-06-13 03:19:38 +0200

    Adding tests

--- worbench/obj/Debug/net8.0/worbench.AssemblyInfo.cs
+++ worbench/obj/Debug/net8.0/worbench.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("worbench")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08519e0b5b918f45766976476eea39c6d99028c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dc0b7cacb49aad8145970031fbb81cbf8ab3aad")]
 [assembly: System.Reflection.AssemblyProductAttribute("worbench")]
 [assembly: System.Reflection.AssemblyTitleAttribute("worbench")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
